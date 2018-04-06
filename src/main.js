48850724e74822eb594b98392a0f3c04a08e3538
daidai <571143755@qq.com>
2018-04-06 22:50:11 +0800

    each函数与测试

--- src/main.js
+++ src/main.js
@@ -1,5 +1,59 @@
+module.exports = {
+  /**
+   * 数组，对象，NodeList遍历
+   * @param obj
+   * @param iterator
+   * @param context
+   * @returns {*}
+   */
+  each : function (obj, iterator, context) {
+    var index = 0;
+    try {
+      if (obj.forEach) {
+        obj.forEach(iterator, context);
+      } else if (obj.length) {
+        for (var i = 0; i < obj.length; i++) {
+          iterator.call(context, obj[i], i);
+        }
+      } else if (obj._each) {
+        obj._each(function (value) {
+          iterator.call(context, value, index++);
+        })
+      } else {
+        var i = 0;
+        for (var key in obj) {
+          var value = obj[key], pair = [key, value];
+          pair.key = key;
+          pair.value = value;
+          iterator.call(context, pair, i++);
+        }
+      }
+    } catch (e) {
+      if (e !== '_break_') {
+        throw e;
+      }
+    }
+    return obj;
+  },
+  /**
+   * 有一个为false都返回false
+   * @param obj
+   * @param iterator
+   * @param context
+   * @returns {boolean}
+   */
+  all : function (obj, iterator, context) {
+    if (obj.every) {
+      obj.every(iterator, context);
+    }
+    var result = true;
+    _.each(obj, function (value, index) {
+      result = result && iterator.call(context, value, index);
+      if (!result) {
+        throw '_break';
+      }
+    });
+    return result;
+  }
+}
 
-console.log("load reverse.js");
-var  Reverse = function(name){
-  return name.split("").reverse().join("");
-};
--- test/mainSpec.js
+++ test/mainSpec.js
@@ -1,5 +1,21 @@
-describe("start",function(){
-  it("should return true",function(){
-    expect(Reverse("abc")).to.equal("cba");
+// var assert = chai.assert;
+var assert = require('assert');
+var _ = require('../src/main.js');
+describe('window._', function() {
+  describe('#each()', function() {
+    it('Array value is equal to (index + 1)', function() {
+      _.each([1, 2, 3], function (value, index) {
+        assert.equal(value, index + 1);
+      });
+    });
+
+    it('Object key is equal to value', function () {
+      _.each({
+        '2': '2',
+        '3': '3'
+      },function (pair, index) {
+        assert.equal(pair.key, pair.value);
+      })
+    });
   });
 });
